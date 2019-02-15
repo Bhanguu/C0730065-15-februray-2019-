56efed5e4c2cabba8a149db77ef0e39f3d7c3f7f
visualstudio <visualstudio@124-PC15>
2019-02-15 18:41:29 -0500

    hlo

---
+++ C0730065(15 februray 2019)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730065_15_februray_2019_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
